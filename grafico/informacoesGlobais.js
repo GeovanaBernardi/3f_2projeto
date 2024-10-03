551a9ad9126d8deaa307cfcf58a03946527ac25f
GeovanaBernardi <104101129+GeovanaBernardi@users.noreply.github.com>
2024-10-03 09:47:41 -0300

    Add files via upload

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -1,15 +1,22 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
+console.log(url);
+
 async function visualizaDadosGlobais(){
- const res = await fetch(url);
- const dados = await res.json();
- console.log(dados);
+    const res = await fetch(url);
+    const dados = await res.json();
+    const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+    const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
+    const tempoMedio = dados.tempo_medio;
+    console.log(dados);
 
- const paragrafo = document.createElement('p')
-
- paragrafo.classList.add('graficos-texto')
- const container = document.getElementById('graficos-container')
-
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-texto');
+    const container = document.getElementById('graficos-container');
+    paragrafo.innerHTML = `O mundo tem ${totalPessoasMundo} bilhões de habitantes, 
+    dos quais ${totalPessoasConectadas} bilhões estão conectados em alguma rede social 
+    por um tempo médio de ${tempoMedio}.`;
+    container.appendChild(paragrafo);
 }
 
 visualizaDadosGlobais();
