ddb944c078e4d1bd1d12bf56deee62503bbf3006
GeovanaBernardi <104101129+GeovanaBernardi@users.noreply.github.com>
2024-10-31 09:38:36 -0300

    31/10

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./comum.js";
+import { getCSS, tickfont} from "./comum.js";
 async function quantidadeDeUsuarios(){
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -27,6 +27,15 @@ async function quantidadeDeUsuarios(){
                 family: getCSS('--font'),
                 size: 30
             }
+        },
+        xaxis:{
+        tickfont: tickfont,
+        title: {
+            text: 'Nome das redes sociais',
+            font:{
+                color: getCSS('--cor-secundaria')
+            }
+        }
         }
     }
     const graficos = document.createElement('div');
